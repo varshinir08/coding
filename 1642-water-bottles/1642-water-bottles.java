68c955aa242ccc8f53013a12567c9c4d22b9759a
Varshini R <156539614+varshinir08@users.noreply.github.com>
2025-01-27 12:09:48 +0530

    Time: 0 ms (100%), Space: 40.3 MB (82.17%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,16 +1,16 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int res = numBottles;
-        int cur_empty = numBottles;
+        int r=0;
+        int q=0;
+        int ans=numBottles;
+        while(numBottles/numExchange!=0)
+        {
+            q=numBottles/numExchange;
+            r=numBottles%numExchange;
+            numBottles=q+r;
+            ans+=q;
 
-        while (cur_empty / numExchange != 0) {
-            int full_drinked = cur_empty / numExchange;
-            int left_empty = cur_empty - full_drinked * numExchange;
-            res += full_drinked;
-            cur_empty = left_empty + full_drinked;
         }
-
-        return res;
-        
+        return ans;
     }
 }
